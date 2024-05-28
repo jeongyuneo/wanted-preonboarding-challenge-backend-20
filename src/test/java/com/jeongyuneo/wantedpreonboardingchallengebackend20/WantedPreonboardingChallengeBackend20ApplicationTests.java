4c9315e4f3315f326d7bf5cdd6fdb736f4d317ac
jeongyuneo <piq2255@gmail.com>
2024-05-28 22:17:21 +0900

    chore: init project

---
+++ src/test/java/com/jeongyuneo/wantedpreonboardingchallengebackend20/WantedPreonboardingChallengeBackend20ApplicationTests.java
@@ -0,0 +1,12 @@
+package com.jeongyuneo.wantedpreonboardingchallengebackend20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WantedPreonboardingChallengeBackend20ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
